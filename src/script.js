e516aa135e16507a505a9737551e88ce8a8ac2ac
jottaveDev <jviktor204@gmail.com>
2023-10-26 16:07:55 -0300

    migrando para sass, otimização css

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 const menuMobile = document.querySelector('.menu-mobile');
 const menu = document.querySelector('.menu');
 const modal = document.querySelector('.modal');
+const animation = document.querySelector('[data-anime]');
 
 function handleMenu() {
   menu.classList.toggle('on');
@@ -12,8 +13,10 @@ function handleMenuButton(target) {
   const menuActive = menuMobile.classList.contains('on');
 
   if (menuActive) {
+    animation.setAttribute('data-anime', 'menuIn');
     target.setAttribute('src', './src/assets/images/icon-menu-close.svg');
   } else {
+    animation.setAttribute('data-anime', 'menuOut');
     target.setAttribute('src', './src/assets/images/icon-menu.svg');
   }
 }
